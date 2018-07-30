25a679d3b931aa6f119bc5d65825ac8671d7c125
SiuKeung <siu_keung@outlook.com>
2018-07-30 14:19:05 +0800

    完成了测试"/xml/speedrecords"的返回的记录中，是否有3条记录是在英国被创造的

--- src/test/java/exercises/RestAssuredExercises5Test.java
+++ src/test/java/exercises/RestAssuredExercises5Test.java
@@ -67,11 +67,13 @@ public class RestAssuredExercises5Test {
 	
 	@Test
 	public void checkThreeRecordsHaveBeenSetByCarsFromTheUK() {
-		
-		given().
-			spec(requestSpec).
-		when().
-		then();
+
+        given().
+                spec(requestSpec).
+                when().
+                get("/xml/speedrecords").
+                then()
+                .body("speedRecords.car.grep{it.@country.equals('UK')}.size()", is(3));
 	}
 	
 	/*******************************************************
